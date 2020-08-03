c1490463f543491e732aa33aaece31dacc537e62
miber66 <maloradyra1985>
2020-08-03 13:34:59 +0500

    5.4. Протеиновая диета [#325422]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int mounth = 0;
+        while (ivan < nik) {
+            ivan *= 3;
+            nik *= 2;
+            mounth += 1;
+            System.out.println(mounth);
+        }
+        return mounth;
+    }
+
+    public static void main(String[] args) {
+        calc(90, 100);
+        System.out.println();
+    }
+}
---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        int mounth = Fitness.calc(95, 90);
+        assertThat(mounth, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        int mounth = Fitness.calc(90, 95);
+        assertThat(mounth, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int mounth = Fitness.calc(50, 95);
+        assertThat(mounth, is(2));
+    }
+}
