9d57c9485227e31f908231e5b33cc9eea99daa67
miber66 <markkhokhlyavin@gmail.com>
2020-10-15 12:29:43 +0500

    5.7. Ипотека [#325424]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100;
+
+        while (amount > 0) {
+            amount = amount + (int) (amount * percent);
+            amount = amount - salary;
+            year++;
+        }
+        return year;
+    }
+}
